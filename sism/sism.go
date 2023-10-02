fc6a7bd633a02cd9ffc18841cb0169847d0d7750
Alexandru Mihai <alexandru.mihai@sap.com>
2023-10-02 15:17:00 +0300

    adding plugins

--- sism/sism.go
+++ sism/sism.go
@@ -3,12 +3,12 @@ package sism
 import "github.com/go-openapi/strfmt"
 
 type SISM_Device struct {
-	Id              string          `json:"sism_id"`
-	Serial          string          `json:"sism_serial"`
-	Planned_removal strfmt.DateTime `json:"planned_removal"`
-	Equi_no         string          `json:"equi_no"`
-	Last_sync       strfmt.DateTime `json:"last_sync"`
-	Device          Device          `json:"device"`
+	Id              string      `json:"sism_id"`
+	Serial          string      `json:"sism_serial"`
+	Planned_removal strfmt.Date `json:"planned_removal"`
+	Equi_no         string      `json:"equi_no"`
+	Last_sync       strfmt.Date `json:"last_sync"`
+	Device          Device      `json:"device"`
 }
 
 type Device struct {
