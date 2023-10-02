798cd7b4fa6fe7200e3fd1a1474661c52269c723
Alexandru Mihai <alexandru.mihai@sap.com>
2023-10-02 14:59:01 +0300

    adding plugins

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/sapcc/netbox-plugins
 
 go 1.20
+
+require github.com/go-openapi/strfmt v0.21.7
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/go-openapi/errors v0.20.3 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	go.mongodb.org/mongo-driver v1.11.3 // indirect
+)
--- netbox-sism/models/sism.go
+++ netbox-sism/models/sism.go
@@ -1,6 +1,19 @@
 package models
 
+import "github.com/go-openapi/strfmt"
+
 type SISM_Device struct {
-	Id     string `json:"sism_id"`
-	Serial string `json:"sism_serial"`
+	Id              string          `json:"sism_id"`
+	Serial          string          `json:"sism_serial"`
+	Planned_removal strfmt.DateTime `json:"planned_removal"`
+	Equi_no         string          `json:"equi_no"`
+	Last_sync       strfmt.DateTime `json:"last_sync"`
+	Device          NestedDevice    `json:"device"`
+}
+
+type NestedDevice struct {
+	Id          int    `json:"id"`
+	Url         string `json:"url"`
+	Name        string `json:"name"`
+	DisplayName string `json:"display_name"`
 }
